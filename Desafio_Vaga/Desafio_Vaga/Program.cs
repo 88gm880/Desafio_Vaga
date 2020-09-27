b5bb6769be94c927e43d5bfc5a56489038de4628
Gustavo Macias Corrêa <gu.mcorrea@gmail.com>
2020-09-27 11:12:26 -0300

    Concluido a construção de caminho do robo. Iniciada construção da horta e adicionar canteiros nela.

--- Desafio_Vaga/Desafio_Vaga/Program.cs
+++ Desafio_Vaga/Desafio_Vaga/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace Desafio_Vaga
 {
@@ -7,10 +8,53 @@ namespace Desafio_Vaga
         static void Main(string[] args)
         {
             
-            Robo robo = new Robo(0, 0, 'O');
-            robo.achaCaminho(4, 0);
+            int initX, initY;
+            Horta horta = new Horta(10, 10);
+            horta.addCanteiro(4, 4);
+            horta.addCanteiro(0, 0);
+            horta.addCanteiro(0, 5);
+            //Console.Write("Posicao inicial\t: (");
+            //Console.ReadLine();
+
+            Robo robo = new Robo(4, 4, 'L');
+
+            foreach (Canteiro c in horta.getCanteiros())
+            {
+                robo.achaCaminho(c.getX(), c.getY());
+            }
+
+            Console.WriteLine(robo.getCaminho());
+            Console.WriteLine("Orientacao final: " + robo.getOrientacao());
         }
 
+    }
+
+    class Horta
+    {
+        private int x, y;
+        private ArrayList canteiros = new ArrayList();
+
+        public Horta(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void addCanteiro(int x, int y)
+        {
+            if(x > this.x || y > this.y)
+            {
+                Console.WriteLine("Posicao de canteiro invalida");
+                return;
+            }
+
+            canteiros.Add(new Canteiro(x, y));
+        }
+
+        public ArrayList getCanteiros()
+        {
+            return canteiros;
+        }
     }
 
     class Robo
@@ -18,7 +62,7 @@ namespace Desafio_Vaga
         private char[] direcoes = { 'N', 'L', 'S', 'O' };
         private int direcao;
         private int x, y;
-        private String caminho = "";
+        private String caminho = "Caminho: ";
 
         public Robo(int x, int y, char direcao)
         {
@@ -31,7 +75,16 @@ namespace Desafio_Vaga
                 Console.WriteLine("Direcao invalida");
                 Environment.Exit(2);
             }
-            
+        }
+
+        public char getOrientacao()
+        {
+            return direcoes[direcao];
+        }
+
+        public String getCaminho()
+        {
+            return caminho;
         }
 
         //retorna x
@@ -48,43 +101,60 @@ namespace Desafio_Vaga
 
         public void achaCaminho(int x, int y)
         {
-            int i, direcao;
+            int i, direcaoRef = Array.IndexOf(direcoes, 'L'); // N L S O N... 
             while(this.x != x || this.y != y)
             {
-                if(this.x == x)
+                if(this.x == x) //após o robo chegar no X correto, será encontrado o caminho até Y
                 {
-                    
-                }
-                else if(this.x < x)
-                {
-                    direcao = Array.IndexOf(direcoes, 'L');
-                    if (this.direcao > direcao)
-                        for (i = 0; i <= this.direcao - direcao; i++)
-                            virarEsq();
+                    if(this.y < y)
+                    {
+                        virar(Array.IndexOf(direcoes, 'N'));
+                        for (i = this.y; i < y; i++)
+                            movimento();
+                    }
                     else
-                        for (i = 0; i < direcao - this.direcao; i++)
-                            virarDir();
+                    {
+                        virar(Array.IndexOf(direcoes, 'S'));
+                        for (i = y; i < this.y; i++)
+                            movimento();
+                    }
+                }
+                else if(this.x < x)// quando o robo estiver a Oeste da posicao do canteiro
+                {
+                    virar(Array.IndexOf(direcoes, 'L'));
                     for (i = this.x; i < x; i++)
                         movimento();
                 }
-                /*else
+                else // quando o robo estiver a Leste da posicao do canteiro
                 {
-                    direcao = Array.IndexOf(direcoes, 'L');
-                    if (this.direcao ecao)
-                        for (i = 0; i <= this.direcao - direcao; i++)
-                            virarEsq();
-                    else
-                        for (i = 0; i < direcao - this.direcao; i++)
-                            virarDir();
+                    virar(Array.IndexOf(direcoes, 'O'));
                     for (i = x; i < this.x; i++)
                         movimento();
-                }*/
-
+                }
             }
             irrigar();
-            Console.WriteLine(this.x +" "+ this.y);
-            Console.WriteLine(caminho);
-            Console.WriteLine(direcoes[this.direcao]);
+        }
+
+        //faz as direcoes que o robo deve virar para chegar de uma direcao para outra, 
+        //o parametro é o indice da direcao desejada
+        public void virar(int direcao)
+        {
+            int aux =(this.direcao - direcao + 4) % 4;
+            switch (aux)
+            {
+                case 0:
+                    return;
+                case 1:
+                    virarEsq();
+                    break;
+                case 2:
+                    virarDir();
+                    virarDir();
+                    break;
+                case 3:
+                    virarDir();
+                    break;
+            }
         }
 
         //Movimenta o robo para frente na direção que ele está virado
@@ -127,4 +197,27 @@ namespace Desafio_Vaga
             caminho += " I";
         }
     }
+
+    class Canteiro
+    {
+        private int x, y;
+
+        public Canteiro(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int getX()
+        {
+            return x;
+        }
+
+        public int getY()
+        {
+            return y;
+        }
+    }
+
+
 }
